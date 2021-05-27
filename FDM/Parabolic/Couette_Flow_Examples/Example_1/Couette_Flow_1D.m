f051833b63cf3d891c9fc4535600358de14c0ada
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2021-05-27 18:38:26 +0530

    Create Couette_Flow_1D.m

---
+++ FDM/Parabolic/Couette_Flow_Examples/Example_1/Couette_Flow_1D.m
@@ -0,0 +1,46 @@
+% Example-1
+% Couette Flow : Flow between 2 infinitely parallel plates
+clear all; clc
+
+% Known Parameters
+N = 101; % No. of grid points
+h = 1; % Distance between the plates (Length of y-domain)
+dy = h/(N-1); % Grid Size
+u_m = 2; % Velocity of Moving plate
+Re = 100; % Reynold's Number (Aribitrary)
+
+% Calculations
+% Since nu = gamma = U*h/Re
+gamma = (u_m*h/Re); % Diffusion Coefficient
+
+% Condition for stability: gamma_y <= (1/2)
+% gamma_y = nu*dt/(dy^2)
+dt = (dy^2)/(2*gamma); % Time Step
+gamma_y = gamma*dt/(dy*dy);
+
+% Initialization
+u = zeros(N,1); % Velocity Matrix
+U = []; % Velocity variation array with time step
+t = 0; % Time
+T = []; % Time array
+
+% Initial & Boundary Conditions
+u(1,1) = u_m; % Moving plate
+u(N,1) = 0; % Stationary plate
+
+% Computation - Explicit Method
+% Forward in Time Central in Space (FTCS)
+n = 0; % Iterator
+while t<=50
+    u_old = u;
+    for i=2:N-1 % For Internal Nodes
+        u(i,1) = u_old(i,1) + gamma_y*(u_old(i+1,1)-2*u_old(i,1)+u_old(i-1,1));
+    end
+    n = n+1;
+    t = t + dt;
+    U = [U;u'];
+    T(end+1) = t;
+end
+u
+n
+t
