429e07aa14df5968e1a4c27b2f720646cebd9a91
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2021-05-29 18:30:31 +0530

    Update Couette_Flow_1D.m

--- FDM/Parabolic/Couette_Flow_Examples/Example_2/Couette_Flow_1D.m
+++ FDM/Parabolic/Couette_Flow_Examples/Example_2/Couette_Flow_1D.m
@@ -1,15 +1,15 @@
-% 1D Couette Flow Example-2
+% 1D Couette Flow Example-2 using Crank-Nicolson Scheme
 % Fixed Plate -> Top Plate
 % Moving Plate -> Bottom Plate
 clear all; clc
 
 % Known parameters
-h = 0.04; % Distance between the plates
+h = 0.04; % Distance between the plates (0.04 m)
 N = 41; % No. of grid points considered
 dy = h/(N-1); % Grid Size
-U_m = 40; % Velocity of Moving Plate
-v = 0.000217; % v = 1/Re; Re = reynolds number
-dt = 0.002; % Time Step
+U_m = 40; % Velocity of Moving Plate (40 m/s)
+v = 0.000217; % v = Uh/Re; Re = reynolds number (m^2/s)
+dt = 2e-3; % Time Step
 gamma_y = v*dt/(dy^2); % Diffusion Factor Coefficient
 
 % Initialization
@@ -17,17 +17,36 @@ t = 0; % Time
 u = zeros(N,1); % Velocity Matrix
 U = []; % Velocities at particular time steps
 T = []; % Particular Time Steps
+n = 0; % Iterator
 
 % Initial & Boundary Conditions
-u(N,1) = U_m; % Moving Plate
-u(1,1) = 0; % Stationary Plate
+u(N,1) = U_m; % Moving Plate (Bottom Plate)
+u(1,1) = 0; % Stationary Plate (Top Plate)
 
 % Computation - Implicit Method
-% Backward in Time, Central in Space (BTCS)
-while t<=1
+% Crank Nicolson Method
+while t<=10
     u_old = u; % u_old at nth time step & u at (n+1)th time step
     for i=2:N-1
-        u(i,1) = (1/(2*gamma_y))*(gamma_y*u(i+1,1)+u(i-1,1)+u_old(i,1));
+        u(i,1) = ((gamma_y/(2+2*gamma_y))*(u(i+1,1)+u(i-1,1)+u_old(i+1,1)+u_old(i-1,1)))+((1-gamma_y)/(1+gamma_y))*u_old(i,1);
     end
+    U = [U;u'];
+    T(end+1) = t;
     t = t+dt; % Time
+    n = n+1; % Iterator
 end
+u
+n
+t
+
+% Plotting
+figure;
+hold on;
+grid on;
+%plot(u,h:-dy:0)
+for i=1:150:n
+    plot(U(i,:),h:-dy:0,'b-')
+end
+xlabel('Velocity (u)'),ylabel('Height (Y)')
+title('Flow between 2 parallel plates')
+hold off;
